5316d5bbf0f685bf95f2fba8c65ab10d2c82750f
CalebStarkey4 <78829195+CalebStarkey4@users.noreply.github.com>
2022-11-23 12:26:35 -0600

    Add files via upload

---
+++ public class Main {.java
@@ -0,0 +1,32 @@
+public class Main {
+    
+}
+
+private static int enterChoice() {
+    System.out.print("\n1 - View an entry\n2 - Update an entry\n3 - Add an entry\n4 - Delete an entry\n5 - End the program")
+    
+}
+
+private static String getMovieID(String title) {
+    return ""
+}
+
+private static int getMovieYear(String id) {
+
+}
+
+private static String getMovieGenre(String id) {
+
+}
+
+private static String getMovieLanguage(String id) {
+
+}
+
+private static int getMovieRating(String id) {
+
+}
+
+private static String getMovieTitle(String id) {
+
+}
