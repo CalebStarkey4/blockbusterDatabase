4d40d34736064a852c8b3e64a195b6cd6388e8a4
Aslanfan <caleb.j.starkey119@gmail.com>
2022-12-04 21:09:25 -0600

    Changed to python

---
+++ Interface.py
@@ -0,0 +1,23 @@
+
+
+def enterChoice():
+    print("\n1 - View an entry\n2 - Update an entry\n3 - Add an entry\n4 - Delete an entry\n5 - End the program")
+    return 
+
+def getMovieID(title):
+    return "test"
+
+def getMovieYear(id):
+    return 0
+
+def getMovieGenre(id):
+    return "test"
+
+def getMovieLanguage(id):
+    return "test"
+
+def getMovieRating(id):
+    return 0
+
+def getMovieTitle(id):
+    return "test"
